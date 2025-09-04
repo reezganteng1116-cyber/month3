fd31f08df946c345969903881ba7904abba3bd38
jaisyurrahman-arch <jaisyurrahman3@gmail.com>
2025-09-04 20:09:59 +0700

    day four

--- script.js
+++ script.js
@@ -155,3 +155,51 @@ function cekNilai() {
 
   document.getElementById("hasil-nilai").textContent = kesimpulan;
 }
+
+ function luasPersegi(sisi) {
+      return sisi * sisi;
+    }
+
+    function hitung() {
+      let nilaiSisi = document.getElementById("sisi").value;
+      let hasil = luasPersegi(nilaiSisi);
+      document.getElementById("hasil-persegi").textContent = 
+        "Luas persegi = " + hasil;
+    }
+
+function aritmatikaSederhana(a, b, c, d, e) {
+      return a + b - c * d / e;
+    }
+
+    function jumlah5Angka() {
+      let a = parseFloat(document.getElementById("a").value);
+      let b = parseFloat(document.getElementById("b").value);
+      let c = parseFloat(document.getElementById("c").value);
+      let d = parseFloat(document.getElementById("d").value);
+      let e = parseFloat(document.getElementById("e").value);
+
+      let hasilTigaAngka = aritmatikaSederhana(a, b, c, d, e);
+
+      document.getElementById("hasil-penjumlahan").textContent =
+        "Hasil dari " + a + " + " + b + " - " + c + " x " + d + " / " + e + " adalah = " + hasilTigaAngka;
+    }
+
+function isEven(angka) {
+      return angka % 2 === 0;
+    }
+
+    function cekAngka() {
+      let nilai = document.getElementById("angka").value;
+      let hasil = isEven(Number(nilai));
+      document.getElementById("hasil").textContent = "hasil-nya adalah" +hasil;
+    }
+
+function konversiSuhu(celcius) {
+  return (celcius * 9/5) + 32;
+}
+
+   function tampilkanCelcius() {
+    let f = document.getElementById("celcius").value;
+    let g = konversiSuhu(f);
+    document.getElementById("derajat").innerText = g + " °F";
+   }
